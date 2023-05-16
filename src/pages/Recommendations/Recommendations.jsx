7ff0854f014f2fa3e24f63be45eb2fe4dcfe6f4f
Aryan Sawale <aryansawale3@gmail.com>
2023-05-16 11:30:57 +0530

    navigate fix

--- src/pages/Recommendations/Recommendations.jsx
+++ src/pages/Recommendations/Recommendations.jsx
@@ -6,11 +6,11 @@ import { AuthContext } from "../../context/AuthContext";
 import { useContext, useState } from "react";
 import { baseURL } from "../../utils/config";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Recommendations = () => {
   const { currentUUID } = useContext(AuthContext);
-
+  const navigate = useNavigate();
   const api = useAxios();
 
   const [users, setUsers] = useState([]);
